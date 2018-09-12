3be23b3d1744cf704c088971e13c84b3318356a5
yfh0918 <yfh0918@sina.cn>
2018-09-12 09:40:51 +0800

    代码优化

--- Polaris_sentinel/src/main/java/com/polaris/sentinel/ServletFilterInitializer.java
+++ Polaris_sentinel/src/main/java/com/polaris/sentinel/ServletFilterInitializer.java
@@ -1,7 +1,9 @@
 package com.polaris.sentinel;
 
+import java.util.EnumSet;
 import java.util.Set;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -12,6 +14,7 @@ public class ServletFilterInitializer implements  ServletContainerInitializer {
 	private final String SENTINEL_SERVLET_FILTER = "sentinelServletFilter";
 
 	public void onStartup(Set<Class<?>> commonFilters, ServletContext servletContext)  throws ServletException {
-		servletContext.addFilter(SENTINEL_SERVLET_FILTER, new CommonFilter());
+		servletContext.addFilter(SENTINEL_SERVLET_FILTER, new CommonFilter())
+					  .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
 	} 
 }
