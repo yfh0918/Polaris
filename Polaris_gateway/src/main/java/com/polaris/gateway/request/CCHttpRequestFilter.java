6400411bb4587ff9f288b0dc51bc05a6e96a926f
yufh <yufh@mwclg.com>
2019-04-12 15:01:52 +0800

    API网管流控优化

--- Polaris_gateway/src/main/java/com/polaris/gateway/request/CCHttpRequestFilter.java
+++ Polaris_gateway/src/main/java/com/polaris/gateway/request/CCHttpRequestFilter.java
@@ -8,14 +8,10 @@ import org.springframework.stereotype.Service;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.cache.RemovalListener;
-import com.google.common.cache.RemovalNotification;
 import com.google.common.util.concurrent.RateLimiter;
-import com.polaris.gateway.GatewayConstant;
-import com.polaris.gateway.support.HttpRequestFilterSupport;
-import com.polaris.comm.Constant;
 import com.polaris.comm.config.ConfClient;
 import com.polaris.comm.util.LogUtil;
+import com.polaris.gateway.GatewayConstant;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.DefaultHttpRequest;
@@ -37,6 +33,7 @@ public class CCHttpRequestFilter extends HttpRequestFilter {
     
     //控制总的流量
 	private volatile RateLimiter totalRateLimiter;
+	private volatile String ip_rate;
 	private volatile String flow_control_rate;
 
 	public CCHttpRequestFilter() {
@@ -46,19 +43,14 @@ public class CCHttpRequestFilter extends HttpRequestFilter {
 		totalRateLimiter = RateLimiter.create(Integer.parseInt(flow_control_rate));
     			
     	//IP单位的缓存
+		ip_rate = ConfClient.get("gateway.cc.rate");
         loadingCache = CacheBuilder.newBuilder()
                 .maximumSize(1000)
-                .expireAfterWrite(2, TimeUnit.SECONDS)
-                .removalListener(new RemovalListener<String, RateLimiter>() {
-                    @Override
-                    public void onRemoval(RemovalNotification<String, RateLimiter> notification) {
-                        logger.debug("key:{} remove from cache", notification.getKey());
-                    }
-                })
+                .expireAfterWrite(1, TimeUnit.SECONDS)
                 .build(new CacheLoader<String, RateLimiter>() {
                     @Override
                     public RateLimiter load(String key) throws Exception {
-                        RateLimiter rateLimiter = RateLimiter.create(Integer.parseInt(ConfClient.get("gateway.cc.rate")));
+                        RateLimiter rateLimiter = RateLimiter.create(Integer.parseInt(ip_rate));
                         logger.debug("RateLimiter for key:{} have been created", key);
                         return rateLimiter;
                     }
@@ -76,6 +68,26 @@ public class CCHttpRequestFilter extends HttpRequestFilter {
                 rateLimiter = (RateLimiter) loadingCache.get(realIp);
             } catch (ExecutionException e) {
             	logger.error(e);
+            	return true;
+            }
+            
+            if (!ConfClient.get("gateway.cc.rate").equals(ip_rate)) {
+            	synchronized(this) {
+            		if (!ConfClient.get("gateway.cc.rate").equals(ip_rate)) {
+            			ip_rate = ConfClient.get("gateway.cc.rate");
+            			loadingCache = CacheBuilder.newBuilder()
+            	                .maximumSize(1000)
+            	                .expireAfterWrite(1, TimeUnit.SECONDS)
+            	                .build(new CacheLoader<String, RateLimiter>() {
+            	                    @Override
+            	                    public RateLimiter load(String key) throws Exception {
+            	                        RateLimiter rateLimiter = RateLimiter.create(Integer.parseInt(ip_rate));
+            	                        logger.debug("RateLimiter for key:{} have been created", key);
+            	                        return rateLimiter;
+            	                    }
+            	                });
+            		}
+            	}
             }
             
             //最大访问速率gateway.cc.rate
@@ -95,12 +107,10 @@ public class CCHttpRequestFilter extends HttpRequestFilter {
                 if (totalRateLimiter.tryAcquire()) {
                     return false;
                 } else {
-                	this.setResultDto(HttpRequestFilterSupport.createResultDto(Constant.MESSAGE_GLOBAL_ERROR));
                     hackLog(logger, GatewayConstant.getRealIp((DefaultHttpRequest) httpObject), "cc", ConfClient.get("gateway.cc.rate"));
                     return true;
                 }
             } else {
-            	this.setResultDto(HttpRequestFilterSupport.createResultDto(Constant.MESSAGE_GLOBAL_ERROR));
                 hackLog(logger, GatewayConstant.getRealIp((DefaultHttpRequest) httpObject), "cc", ConfClient.get("gateway.cc.rate"));
                 return true;
             }
@@ -108,3 +118,4 @@ public class CCHttpRequestFilter extends HttpRequestFilter {
         return false;
     }
 }
+
