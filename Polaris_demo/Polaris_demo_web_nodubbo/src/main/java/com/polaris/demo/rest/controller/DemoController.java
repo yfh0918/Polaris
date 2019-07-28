55d4f07d95433b88282ea1765105a0d26ae4f035
yfh0918 <yfh0918@sina.cn>
2019-07-28 22:09:17 +0800

    resteasy for swagger https://github.com/swagger-api/swagger-core/wiki/Swagger-Core-RESTEasy-2.X-Project-Setup-1.5

--- Polaris_demo/Polaris_demo_web_nodubbo/src/main/java/com/polaris/demo/rest/controller/DemoController.java
+++ Polaris_demo/Polaris_demo_web_nodubbo/src/main/java/com/polaris/demo/rest/controller/DemoController.java
@@ -4,14 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+//import io.swagger.models.Response;
 
 /**
  * 认证模块
@@ -19,7 +24,7 @@ import io.swagger.annotations.ApiOperation;
  * @return
  */
 @Component
-@Api(value = "/rest/demo", description = "IOrderServiceApi Resource", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+@Api(value = "/rest/demo", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
 @Path("/rest/demo")
 public class DemoController {
 
@@ -40,6 +45,16 @@ public class DemoController {
 
         
         return "hello1234";
+    }
+    
+    @GET
+    @Path("/{param}")
+    @ApiOperation(value = "Returns param", notes = "Returns param", response = DemoController.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful retrieval of param value", response = DemoController.class) })
+    public Response printMessage(@PathParam("param") String msg) {
+        String result = "Hello " + msg + "!";
+        return Response.status(200).entity(result).build();
     }
     
 //    public static void main( String[] args ) throws Exception {
