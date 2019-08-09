443f25235d84147f911bafe9b46723cc8bff212b
yufh <yufh@mwclg.com>
2019-08-09 14:28:25 +0800

    代码优化

--- Polaris_container/Polaris_container_http/src/main/java/com/polaris/http/filter/FilterInitializer.java
+++ Polaris_container/Polaris_container_http/src/main/java/com/polaris/http/filter/FilterInitializer.java
@@ -13,27 +13,7 @@ import com.polaris.core.config.ConfClient;
 public class FilterInitializer implements  ServletContainerInitializer { 
 	private final String POLARIS_REQUEST_FIRST_FILTER = "PolarisRequestFirstFilter";
 	private final String POLARIS_FLOW_CONTROL_FILTER = "PolarisFlowControlFilter";
-//	private final static Map<String, String> initParameterMap = new HashMap<>();
-//	private final static List<ServletContextListener> listenerList = new ArrayList<>();
-//	private final static Map<String, Class <? extends Servlet>> servletClassMap = new HashMap<>();
-//	private final static Map<String, String[]> servletUrlMap = new HashMap<>();
-//	
-//	static {
-//		//initParamete
-//		initParameterMap.put("resteasy.providers", "com.polaris.http.exception.RestExceptionHandler");
-//		initParameterMap.put("contextConfigLocation", "classpath:META-INF\\spring\\applicationContext.xml");
-//		
-//		//listener
-//		listenerList.add(new ResteasyBootstrap());
-//		listenerList.add(new SpringContextLoaderListener());
-//		
-//		//servlet
-//		String[] urls = {"/api/*","/rest/*"};
-//		servletClassMap.put("Resteasy", org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher.class);
-//		servletUrlMap.put("Resteasy", urls);
-//	}
 
-	// listener目前只支持tomcat
 	public void onStartup(Set<Class<?>> requestFirstFilters, ServletContext servletContext)  throws ServletException {
 		
 		// filter
