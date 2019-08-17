9c65dcb65d7f4d7e2a4d8873fc6d6bef603f4870
yfh0918 <yfh0918@sina.cn>
2019-08-17 22:38:46 +0800

    代码优化

---
+++ Polaris_container/Polaris_container_springmvc/src/main/java/com/polaris/springmvc/Initializer.java
@@ -0,0 +1,40 @@
+package com.polaris.springmvc;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import com.polaris.http.initializer.AbsHttpInitializer;
+
+public class Initializer extends  AbsHttpInitializer { 
+
+	@Override
+	public void addInitParameter() {
+		servletContext.setInitParameter("contextConfigLocation", "classpath:META-INF\\spring\\applicationContext.xml");
+		servletContext.setInitParameter("spring.profiles.default", "production");
+	}
+
+	@Override
+	public void addListener() {
+		servletContext.addListener(org.springframework.web.context.request.RequestContextListener.class);
+		servletContext.addListener(org.springframework.web.context.ContextLoaderListener.class);
+	}
+
+	@Override
+	public void addFilter() {
+		FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter", org.springframework.web.filter.CharacterEncodingFilter.class);
+		filter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+		filter.setInitParameter("encoding", "UTF-8");
+		filter.setInitParameter("forceEncoding", "true");
+	}
+
+	@Override
+	public void addServlet() {
+		servletContext.
+	    addServlet("dispatcher", org.springframework.web.servlet.DispatcherServlet.class).
+	    addMapping("/*");	
+	} 
+	
+
+}
