01a2e5e1851e72aeeb1c983dbfdc6023d216d009
fenghua.yu <fenghua.yu@hcit.ai>
2019-09-29 13:43:38 +0800

    bug

--- Polaris_workflow/Polaris_workflow/src/main/java/Application.java
+++ Polaris_workflow/Polaris_workflow/src/main/java/Application.java
@@ -4,6 +4,6 @@ import com.polaris.dubbo.supports.MainSupport;
 
 public class Application {
 	public static void main(String[] args) throws Exception { 
-		MainSupport.startDubboServer(args);
+		MainSupport.startDubboServer(args,null);
     } 
 }
