ce8b4c54fc9fd59f7757bf0a86efdf8755260e2e
fenghua.yu <fenghua.yu@hcit.ai>
2019-10-09 10:39:44 +0800

    当开启scanclasspath的时候防止重复启动

--- Polaris_container/Polaris_container_springmvc/src/main/java/com/polaris/springmvc/WebAppInitializer.java
+++ Polaris_container/Polaris_container_springmvc/src/main/java/com/polaris/springmvc/WebAppInitializer.java
@@ -1,11 +1,24 @@
 package com.polaris.springmvc;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.polaris.http.initializer.WebConfigInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	private static boolean initialized = false;
 
+	@Override
+	public synchronized void onStartup(ServletContext servletContext) throws ServletException {
+		if (initialized) {
+			return;
+		}
+		initialized = true;
+		super.onStartup(servletContext);
+	}
+	
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return WebConfigInitializer.getRootConfigs();
@@ -20,5 +33,10 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
+	
+	public static boolean isInitialized () {
+		return initialized;
+	}
+	
 
 }
