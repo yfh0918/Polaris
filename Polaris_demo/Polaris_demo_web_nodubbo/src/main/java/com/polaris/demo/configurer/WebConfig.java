a40d3c05f714ffac973d93829d8796a8dfb64a9b
fenghua.yu <fenghua.yu@hcit.ai>
2019-12-03 14:34:41 +0800

    demo

--- Polaris_demo/Polaris_demo_web_nodubbo/src/main/java/com/polaris/demo/configurer/WebConfig.java
+++ Polaris_demo/Polaris_demo_web_nodubbo/src/main/java/com/polaris/demo/configurer/WebConfig.java
@@ -1,42 +1,51 @@
-package com.polaris.demo.configurer;
-
+//package com.polaris.demo.configurer;
+//
+//import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.ViewResolver;
+//import org.springframework.web.multipart.MultipartResolver;
+//import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 //import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
 //
 //@Configuration
 //@EnableWebMvc 
-//@ComponentScan("com.polaris.demo.rest.controller")
 //public class WebConfig extends WebMvcConfigurerAdapter {
 //	
+//	@Value("${uploadFilePath}")
+//	private String uploadFilePath;
 //	
-//	@Bean
-//    public ViewResolver viewResolver() {
-//		 InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-//		 resolver.setPrefix("/WEB-INF/views/");
-//		 resolver.setSuffix(".jsp");
-//		 resolver.setExposeContextBeansAsAttributes(true);
-//		 return resolver;
-//	}
-//
+//	@Value("${uploadImgPath}")
+//	private String uploadImgPath;
+//	
+//	@Bean(name = "multipartResolver")
+//    public MultipartResolver multipartResolver() {
+//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//        multipartResolver.setDefaultEncoding("UTF-8");
+//        multipartResolver.setMaxUploadSize(1024*1024*1024);//1G
+//        multipartResolver.setMaxInMemorySize(40960);
+//        return multipartResolver;
+//    }
+//	
 //	@Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //	    
+//		//支持静态资源访问
+//		registry.addResourceHandler("/**")
+//        	.addResourceLocations("classpath:/resources/")
+//        	.addResourceLocations("classpath:/static/")
+//        	.addResourceLocations("classpath:/public/");
+//		
+//		//静态文件映射
 //        registry.addResourceHandler("/resource/img/**")
-//                .addResourceLocations("file:/var/www/resources/upload/distribution/img/");
-//        
+//        	.addResourceLocations("file:"+uploadImgPath);
 //        registry.addResourceHandler("/resource/file/**")
-//        .addResourceLocations("file:/var/www/resources/upload/distribution/file/");
+//        	.addResourceLocations("file:"+uploadFilePath);
 //        super.addResourceHandlers(registry);
 //    }
 //
-//	 //配置静态资源的处理
 //	 @Override
 //	 public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 //		 configurer.enable();
