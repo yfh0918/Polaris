93fd46c662309e51173693ef7517aae8c522f257
fenghua.yu <fenghua.yu@hcit.ai>
2020-01-02 13:48:21 +0800

    bug修改

--- Polaris_dubbo/src/main/java/com/polaris/dubbo/filter/SentinelDubboConsumerFilter.java
+++ Polaris_dubbo/src/main/java/com/polaris/dubbo/filter/SentinelDubboConsumerFilter.java
@@ -15,6 +15,9 @@
  */
 package com.polaris.dubbo.filter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.Filter;
 import org.apache.dubbo.rpc.Invocation;
@@ -54,7 +57,16 @@ public class SentinelDubboConsumerFilter extends AbstractDubboFilter implements 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
     	//传递参数
-    	RpcContext.getContext().setAttachments(GlobalContext.getContext());
+    	Map<String,Object> globalContext = GlobalContext.getContext();
+    	Map<String, String> attachments = new HashMap<>();
+    	for (Map.Entry<String, Object> entry : globalContext.entrySet()) {
+    		if (!entry.getKey().equals(GlobalContext.PARENT_ID) &&
+    			!entry.getKey().equals(GlobalContext.TRACE_ID) &&
+    			!entry.getKey().equals(GlobalContext.REQUEST)) {
+    			attachments.put(entry.getKey(), entry.getValue().toString());
+    		}
+    	}
+    	RpcContext.getContext().setAttachments(attachments);
     	RpcContext.getContext().setAttachment(GlobalContext.PARENT_ID, GlobalContext.getModuleId());
     	RpcContext.getContext().setAttachment(GlobalContext.TRACE_ID, GlobalContext.getTraceId());
 
