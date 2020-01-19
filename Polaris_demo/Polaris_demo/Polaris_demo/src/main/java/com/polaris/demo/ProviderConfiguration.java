4d6795c5d4bb2b6d234c3301421e7d05df31f541
fenghua.yu <fenghua.yu@hcit.ai>
2020-01-19 17:59:57 +0800

    demo更新

--- Polaris_demo/Polaris_demo/Polaris_demo/src/main/java/com/polaris/demo/ProviderConfiguration.java
+++ Polaris_demo/Polaris_demo/Polaris_demo/src/main/java/com/polaris/demo/ProviderConfiguration.java
@@ -1,62 +1,62 @@
-//package com.polaris.demo;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import org.apache.dubbo.config.MethodConfig;
-//import org.apache.dubbo.config.ServiceConfig;
-//import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import com.polaris.demo.api.service.DemoEntryIF;
-//import com.polaris.demo.core.entry.DemoEntry;
-//
-//@Configuration
-//public class ProviderConfiguration {
-//	
+package com.polaris.demo;
+import java.util.ArrayList;
+import java.util.List;
 
-////    //<dubbo:protocol name="dubbo" port="20882"></dubbo:protocol>
-////    @Bean
-////    @Primary
-////    public ProtocolConfig protocolConfig() {
-////        ProtocolConfig protocolConfig = new ProtocolConfig();
-////        protocolConfig.setName(ConfClient.get("dubbo.protocol.name"));
-////        protocolConfig.setPort(Integer.parseInt(ConfClient.get("dubbo.protocol.port")));
-////        return protocolConfig;
-////    }
-//    
-//    /**
-//    <dubbo:service interface="com.polaris.demo.api.service.DemoEntryIF" ref="demoEntry" timeout="1000" version="1.0.0">
-//        <dubbo:method name="test" timeout="1000"/>
-//    </dubbo:service>
-//     */
-////    @Bean
-////    @Primary
-////    public ServiceConfig<DemoEntryIF> serviceConfig(DemoEntry demoEntry){
-////        ServiceConfig<DemoEntryIF> serviceConfig = new ServiceConfig<>();
-////        serviceConfig.setInterface(DemoEntryIF.class);
-////        serviceConfig.setRef(demoEntry);
-////        serviceConfig.setVersion("1.0.0");
-////        
-////        //配置每一个method的信息
-////        MethodConfig methodConfig1 = new MethodConfig();
-////        methodConfig1.setName("test");
-////        methodConfig1.setTimeout(1000);
-////
-////        MethodConfig methodConfig2 = new MethodConfig();
-////        methodConfig2.setName("test2");
-////        methodConfig2.setTimeout(1000);
-////        
-////        //将method的设置关联到service配置中
-////        List<MethodConfig> methods = new ArrayList<>();
-////        methods.add(methodConfig1);
-////        methods.add(methodConfig2);
-////        serviceConfig.setMethods(methods);
-////        
-////        //ProviderConfig
-////        //MonitorConfig
-////        
-////        return serviceConfig;
-////    }
-//}
+import org.apache.dubbo.config.MethodConfig;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.ServiceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.polaris.core.config.ConfClient;
+import com.polaris.demo.api.service.DemoEntryIF;
+import com.polaris.demo.core.entry.DemoEntry;
+
+
+@Configuration
+public class ProviderConfiguration {
+
+    //<dubbo:protocol name="dubbo" port="20882"></dubbo:protocol>
+    @Bean
+    @Primary
+    public ProtocolConfig protocolConfig() {
+        ProtocolConfig protocolConfig = new ProtocolConfig();
+        protocolConfig.setName(ConfClient.get("dubbo.protocol.name"));
+        protocolConfig.setPort(Integer.parseInt(ConfClient.get("dubbo.protocol.port")));
+        return protocolConfig;
+    }
+    
+    /**
+    <dubbo:service interface="com.polaris.demo.api.service.DemoEntryIF" ref="demoEntry" timeout="1000" version="1.0.0">
+        <dubbo:method name="test" timeout="1000"/>
+    </dubbo:service>
+     */
+    @Bean
+    public ServiceConfig<DemoEntryIF> serviceConfig(DemoEntry demoEntry){
+        ServiceConfig<DemoEntryIF> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(DemoEntryIF.class);
+        serviceConfig.setRef(demoEntry);
+        serviceConfig.setVersion("1.0.0");
+        
+        //配置每一个method的信息
+        MethodConfig methodConfig1 = new MethodConfig();
+        methodConfig1.setName("test");
+        methodConfig1.setTimeout(1000);
+
+        MethodConfig methodConfig2 = new MethodConfig();
+        methodConfig2.setName("test2");
+        methodConfig2.setTimeout(1000);
+        
+        //将method的设置关联到service配置中
+        List<MethodConfig> methods = new ArrayList<>();
+        methods.add(methodConfig1);
+        methods.add(methodConfig2);
+        serviceConfig.setMethods(methods);
+        
+        //ProviderConfig
+        //MonitorConfig
+        
+        return serviceConfig;
+    }
+}
--- Polaris_demo/Polaris_demo_web/src/main/java/com/polaris/demo/configurer/ConsumerConfiguration.java
+++ Polaris_demo/Polaris_demo_web/src/main/java/com/polaris/demo/configurer/ConsumerConfiguration.java
@@ -1,51 +1,50 @@
-//package com.polaris.demo.configurer;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import org.apache.dubbo.config.ApplicationConfig;
-//import org.apache.dubbo.config.MethodConfig;
-//import org.apache.dubbo.config.ReferenceConfig;
-//import org.apache.dubbo.config.RegistryConfig;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import com.polaris.demo.api.service.DemoEntryIF;
-//
-//@Configuration
-//public class ConsumerConfiguration {
+package com.polaris.demo.configurer;
 
-//    @Bean
-//    @Primary
-//    public DemoEntryIF demoEntryIF(ApplicationConfig applicationConfig, RegistryConfig registryConfig){
-//    	ReferenceConfig<DemoEntryIF> referenceConfig = new ReferenceConfig<>();
-//    	
-//    	referenceConfig.setApplication(applicationConfig);
-//        referenceConfig.setRegistry(registryConfig);
-//        
-//    	referenceConfig.setInterface(DemoEntryIF.class);
-//    	referenceConfig.setVersion("1.0.0");
-//    	referenceConfig.setUrl("dubbo://192.168.1.13:20880");
-//        
-//        //配置每一个method的信息
-//        MethodConfig methodConfig1 = new MethodConfig();
-//        methodConfig1.setName("test");
-//        methodConfig1.setTimeout(1000);
-//
-//        MethodConfig methodConfig2 = new MethodConfig();
-//        methodConfig2.setName("test2");
-//        methodConfig2.setTimeout(1000);
-//        
-//        //将method的设置关联到service配置中
-//        List<MethodConfig> methods = new ArrayList<>();
-//        methods.add(methodConfig1);
-//        methods.add(methodConfig2);
-//        referenceConfig.setMethods(methods);
-//        
-//        //ProviderConfig
-//        //MonitorConfig
-//        
-//        return referenceConfig.get();
-//    }
-//    
-//}
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.MethodConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.polaris.demo.api.service.DemoEntryIF;
+
+@Configuration
+public class ConsumerConfiguration {
+
+    @Bean
+    public DemoEntryIF demoEntryIF(ApplicationConfig applicationConfig, RegistryConfig registryConfig){
+    	ReferenceConfig<DemoEntryIF> referenceConfig = new ReferenceConfig<>();
+    	
+    	referenceConfig.setApplication(applicationConfig);
+        referenceConfig.setRegistry(registryConfig);
+        
+    	referenceConfig.setInterface(DemoEntryIF.class);
+    	referenceConfig.setVersion("1.0.0");
+    	referenceConfig.setUrl("dubbo://192.168.1.13:20880");
+        
+        //配置每一个method的信息
+        MethodConfig methodConfig1 = new MethodConfig();
+        methodConfig1.setName("test");
+        methodConfig1.setTimeout(1000);
+
+        MethodConfig methodConfig2 = new MethodConfig();
+        methodConfig2.setName("test2");
+        methodConfig2.setTimeout(1000);
+        
+        //将method的设置关联到service配置中
+        List<MethodConfig> methods = new ArrayList<>();
+        methods.add(methodConfig1);
+        methods.add(methodConfig2);
+        referenceConfig.setMethods(methods);
+        
+        //ProviderConfig
+        //MonitorConfig
+        
+        return referenceConfig.get();
+    }
+    
+}
