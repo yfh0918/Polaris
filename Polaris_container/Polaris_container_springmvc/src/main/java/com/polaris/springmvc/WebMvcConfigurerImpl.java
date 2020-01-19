ef5bbbc81c364405d9d9f9e31f0e56494aaa3b62
fenghua.yu <fenghua.yu@hcit.ai>
2020-01-19 14:19:17 +0800

    完善springmvc配置

---
+++ Polaris_container/Polaris_container_springmvc/src/main/java/com/polaris/springmvc/WebMvcConfigurerImpl.java
@@ -0,0 +1,86 @@
+package com.polaris.springmvc;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import com.polaris.core.config.ConfClient;
+
+@Configuration 
+@EnableWebMvc
+public class WebMvcConfigurerImpl extends WebMvcConfigurerAdapter {
+	
+	@Bean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setDefaultEncoding(Charset.defaultCharset().toString());
+        long maxUploadSize = Long.parseLong(ConfClient.get("multipart.maxUploadSize","1073741824"));
+        multipartResolver.setMaxUploadSize(maxUploadSize);//1G
+        int maxInMemorySize = Integer.parseInt(ConfClient.get("multipart.maxInMemorySize","40960"));
+        multipartResolver.setMaxInMemorySize(maxInMemorySize);
+        return multipartResolver;
+    }
+	
+	/**
+       * 
+       * Description：FreeMarker视图配置
+       * @return 
+       */
+	@Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+		FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+		String templateLoaderPath = ConfClient.get("freeMarker.templateLoaderPath", "classpath:template/");
+		freeMarkerConfigurer.setTemplateLoaderPath(templateLoaderPath);
+		freeMarkerConfigurer.setDefaultEncoding(Charset.defaultCharset().toString());
+	    return freeMarkerConfigurer;
+   }
+
+    @Bean
+    public FreeMarkerViewResolver freeMarkerViewResolver() {
+	    FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+	    String freeMarkerViewResolverSuffix = ConfClient.get("freeMarker.viewResolver.suffix", ".ftl");
+	    viewResolver.setSuffix(freeMarkerViewResolverSuffix);
+	    viewResolver.setCache(true);
+	    viewResolver.setContentType("text/html;charset="+Charset.defaultCharset().toString());
+	    viewResolver.setExposeRequestAttributes(true);
+	    viewResolver.setExposeSessionAttributes(true);
+	    viewResolver.setRequestContextAttribute("request");
+	    viewResolver.setOrder(0);
+	    return viewResolver;
+   }
+    
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    	
+    	//string utf-8
+        converters.add(new StringHttpMessageConverter(Charset.defaultCharset()));
+        
+        //pojo utf-8
+    	List<MediaType> list = new ArrayList<MediaType>();
+    	list.add(MediaType.APPLICATION_JSON_UTF8);
+    	MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+    	mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+        converters.add(mappingJackson2HttpMessageConverter);
+    }
+
+}
