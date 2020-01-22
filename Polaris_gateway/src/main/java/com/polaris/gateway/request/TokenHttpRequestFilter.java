8831d29411ff87f54ebf697b48f3bc35b89463f1
fenghua.yu <fenghua.yu@hcit.ai>
2020-01-22 12:12:11 +0800

    优化

--- Polaris_gateway/src/main/java/com/polaris/gateway/request/TokenHttpRequestFilter.java
+++ Polaris_gateway/src/main/java/com/polaris/gateway/request/TokenHttpRequestFilter.java
@@ -5,16 +5,15 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
 import com.polaris.core.Constant;
 import com.polaris.core.config.ConfClient;
 import com.polaris.core.config.ConfHandlerSupport;
 import com.polaris.core.config.ConfListener;
 import com.polaris.core.util.JwtUtil;
 import com.polaris.core.util.StringUtil;
-import com.polaris.gateway.GatewayConstant;
 import com.polaris.gateway.support.HttpRequestFilterSupport;
 
+import cn.hutool.core.date.SystemClock;
 import cn.hutool.core.util.StrUtil;
 import io.jsonwebtoken.Claims;
 import io.netty.channel.ChannelHandlerContext;
@@ -118,6 +117,8 @@ public class TokenHttpRequestFilter extends HttpRequestFilter {
             //认证
             String token = httpRequest.headers().get(Constant.TOKEN_ID);
             try {
+            	
+            	//token认证
                 if (StringUtil.isEmpty(token)) {
                 	this.setResultDto(HttpRequestFilterSupport.createResultDto(Constant.TOKEN_FAIL_CODE,TOKEN_MESSAGE));
                     return true;
@@ -132,8 +133,14 @@ public class TokenHttpRequestFilter extends HttpRequestFilter {
                 	this.setResultDto(HttpRequestFilterSupport.createResultDto(Constant.TOKEN_FAIL_CODE,TOKEN_MESSAGE));
                     return true;
                 }
-                //claim对象
-                httpRequest.headers().add(GatewayConstant.CLAIMS, JSON.toJSONString(claims));
+                
+                //过期时间超过不能超过5秒
+                long expiration = claims.getExpiration().getTime();
+                long now = SystemClock.now() + 1000*5;
+                if (now >= expiration) {
+                	return true;
+                }
+                
                 return false;
             } catch (Exception ex) {
             	this.setResultDto(HttpRequestFilterSupport.createResultDto(Constant.TOKEN_FAIL_CODE,TOKEN_MESSAGE));
