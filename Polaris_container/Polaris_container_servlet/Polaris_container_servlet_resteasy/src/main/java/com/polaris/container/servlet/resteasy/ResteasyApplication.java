adbdc90b82cc451936d95b95a06abf88467610ef
yfh0918 <yfh0918@sina.cn>
2020-01-26 15:29:53 +0800

    代码结构调整

---
+++ Polaris_container/Polaris_container_servlet/Polaris_container_servlet_resteasy/src/main/java/com/polaris/container/servlet/resteasy/ResteasyApplication.java
@@ -0,0 +1,44 @@
+package com.polaris.container.servlet.resteasy;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+import com.polaris.core.util.SpringUtil;
+
+public class ResteasyApplication extends Application {
+	private static Set<Object> singletons = new HashSet<Object>();
+    private static Set<Class<?>> classes = new HashSet<Class<?>>();
+ 
+    public ResteasyApplication() {
+    	
+		Map<String,Object> paths = SpringUtil.getApplicationContext().getBeansWithAnnotation(Path.class);
+		if (paths != null && paths.size() > 0) {
+	        for (Object path : paths.values()) {
+	        	singletons.add(path);
+	        	classes.add(path.getClass());
+	        }
+		}
+    }
+ 
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+ 
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+    
+    public static void registSingleton(Object obj) {
+    	Path path = obj.getClass().getAnnotation(Path.class);
+    	if (path != null) {
+        	singletons.add(obj);
+        	classes.add(obj.getClass());
+    	}
+    }
+}
