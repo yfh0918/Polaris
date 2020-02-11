13e3b9c7cd1b999ae809034bc2306f11c7a5ce08
fenghua.yu <fenghua.yu@hcit.ai>
2020-02-11 13:05:06 +0800

    更新demo

--- Polaris_demo/Polaris_demo_web_springmvc/src/main/java/com/polaris/demo/config/WebConfigurer.java
+++ Polaris_demo/Polaris_demo_web_springmvc/src/main/java/com/polaris/demo/config/WebConfigurer.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.polaris.extension.mvc.AbstractMVCConfigurerEventListener;
+import com.polaris.extension.mvc.MVCConfigurerEvent;
+
 @Configuration
-public class WebConfigurer implements WebMvcConfigurer {
+public class WebConfigurer extends AbstractMVCConfigurerEventListener implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -14,5 +17,26 @@ public class WebConfigurer implements WebMvcConfigurer {
         	.addResourceLocations("classpath:/resources/")
         	.addResourceLocations("classpath:/static/")
         	.addResourceLocations("classpath:/public/");
+	}
+
+	@Override
+	protected void onMVCEvent(MVCConfigurerEvent event) {
+		
+//		//重新构建
+//		if (event.getEventType() == HttpMessageConverter.class) {
+//			List<HttpMessageConverter<?>> converters = event.getEventParameter(List.class);
+//			event.setEventResult(converters);
+//		} else if (event.getEventType() == MultipartResolver.class) {
+//			CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//			event.setEventResult(multipartResolver);
+//		} else if (event.getEventType() == FreeMarkerConfigurer.class) {
+//			FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+//			event.setEventResult(freeMarkerConfigurer);
+//			
+//		} else if (event.getEventType() == FreeMarkerViewResolver.class) {
+//			FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+//			event.setEventResult(viewResolver);
+//		}
+		
 	}
 }
