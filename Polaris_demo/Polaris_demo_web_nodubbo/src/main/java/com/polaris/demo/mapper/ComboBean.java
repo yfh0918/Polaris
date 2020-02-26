7e28888b5c6161906be1bf0746249c3373330a0c
fenghua.yu <fenghua.yu@hcit.ai>
2020-02-26 16:31:08 +0800

    更新

--- Polaris_demo/Polaris_demo_web_nodubbo/src/main/java/com/polaris/demo/mapper/ComboBean.java
+++ Polaris_demo/Polaris_demo_web_nodubbo/src/main/java/com/polaris/demo/mapper/ComboBean.java
@@ -1,33 +1,16 @@
 package com.polaris.demo.mapper;
 
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.math.BigDecimal;
 import java.util.Date;
 
-/**
- * @Author: lei.chen@hcit.ai
- * @Description:
- * @CreateTiem: 2019/11/28 15:37
- **/
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 @Table(name = "combo_tbl")
 public class ComboBean {
     @Id
     private Long comboId;
-    //套餐名称
     private String comboName;
-    
-    //套餐描述
-    private String comboDesc;
-
-    //可使用次数
-    private Integer availableUploads;
-
-
-    private BigDecimal comboAmt;
-    
-    private Integer comboType;
-    
     private Date createTime;
     private Date updateTime;
 
@@ -49,21 +32,7 @@ public class ComboBean {
 		this.comboName = comboName;
 	}
 
-	public String getComboDesc() {
-        return comboDesc;
-    }
 
-    public void setComboDesc(String comboDesc) {
-        this.comboDesc = comboDesc;
-    }
-
-    public Integer getAvailableUploads() {
-        return availableUploads;
-    }
-
-    public void setAvailableUploads(Integer availableUploads) {
-        this.availableUploads = availableUploads;
-    }
 
     public Date getCreateTime() {
         return createTime;
@@ -81,19 +50,5 @@ public class ComboBean {
         this.updateTime = updateTime;
     }
 
-    public BigDecimal getComboAmt() {
-        return comboAmt;
-    }
 
-    public void setComboAmt(BigDecimal comboAmt) {
-        this.comboAmt = comboAmt;
-    }
-
-	public Integer getComboType() {
-		return comboType;
-	}
-
-	public void setComboType(Integer comboType) {
-		this.comboType = comboType;
-	}
 }
