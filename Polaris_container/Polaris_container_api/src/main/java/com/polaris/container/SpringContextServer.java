7e06cd8adf951ced47d980c7ed6cfb34ec28f6c2
yfh0918 <yfh0919@icloud.com>
2020-06-03 09:59:38 +0800

    1.1.3-代码优化

--- Polaris_container/Polaris_container_api/src/main/java/com/polaris/container/SpringContextServer.java
+++ Polaris_container/Polaris_container_api/src/main/java/com/polaris/container/SpringContextServer.java
@@ -7,10 +7,12 @@ import com.polaris.core.util.SpringUtil;
 
 public class SpringContextServer implements Server{
 	
+    @Override
     public void start() throws Exception {
     	SpringUtil.refresh(ConfigurationHelper.getConfiguration());
     }
     
+    @Override
     public void stop() throws Exception {
     	ConfigurableApplicationContext context = SpringUtil.getApplicationContext();
         if (context != null) {
@@ -18,6 +20,7 @@ public class SpringContextServer implements Server{
         }
     }
     
+    @Override
     public Object getContext() {
     	return SpringUtil.getApplicationContext();
     }
