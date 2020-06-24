4953bc6b6cd88ad2e7c9b2632233ae7117cab559
yfh0918 <yfh0919@icloud.com>
2020-06-24 15:42:20 +0800

    1.3.2

--- Polaris_core/src/main/java/com/polaris/core/naming/NamingUrlHandler.java
+++ Polaris_core/src/main/java/com/polaris/core/naming/NamingUrlHandler.java
@@ -1,5 +1,23 @@
 package com.polaris.core.naming;
 
-public interface NamingUrlHandler extends NamingHandler, UrlHandler {
+import java.util.List;
 
+public interface NamingUrlHandler extends NamingHandler {
+    /**
+     * 获取真实的IP+url
+     * @param 
+     * @return 
+     * @Exception 
+     * @since 
+     */
+     String getRealIpUrl(String serviceNameUrl);
+     
+     /**
+     * 获取真实的IP+url 列表
+     * @param 
+     * @return 
+     * @Exception 
+     * @since 
+     */
+     List<String> getRealIpUrlList(String serviceNameUrl);
 }
