d80bc27a6134d426bba96efcee267f3bf5ed21da
yfh0918 <yfh0919@icloud.com>
2020-08-06 13:17:05 +0800

    代码优化

--- Polaris_demo/Polaris_demo_gateway/src/main/java/com/polaris/demo/gateway/GatewayApplication.java
+++ Polaris_demo/Polaris_demo_gateway/src/main/java/com/polaris/demo/gateway/GatewayApplication.java
@@ -3,8 +3,12 @@ package com.polaris.demo.gateway;
 
 import com.polaris.container.ServerRunner;
 import com.polaris.container.annotation.PolarisApplication;
+import com.polaris.container.gateway.HttpFilterHelper;
+import com.polaris.container.gateway.pojo.HttpFilterEntityEnum;
 import com.polaris.container.listener.ServerListener;
 import com.polaris.core.component.LifeCycle;
+import com.polaris.demo.gateway.request.TokenExtendHttpRequestFilter;
+import com.polaris.demo.gateway.response.TokenExtendHttpResponseFilter;
 
 @PolarisApplication
 public class GatewayApplication {
@@ -16,8 +20,8 @@ public class GatewayApplication {
     		@Override
     		public void started(LifeCycle event) {
                 //HttpFilterHelper.INSTANCE.removeFilter(HttpFilterEntityEnum.CC.getFilterEntity());
-    			//HttpFilterHelper.INSTANCE.replaceFilter(HttpFilterEntityEnum.Token.getFilterEntity(), new TokenExtendHttpRequestFilter());
-    			//HttpFilterHelper.INSTANCE.replaceFilter(HttpFilterEntityEnum.TokenResponse.getFilterEntity(), new TokenExtendHttpResponseFilter());
+    			HttpFilterHelper.INSTANCE.replaceFilter(HttpFilterEntityEnum.Token.getFilterEntity(), new TokenExtendHttpRequestFilter());
+    			HttpFilterHelper.INSTANCE.replaceFilter(HttpFilterEntityEnum.TokenResponse.getFilterEntity(), new TokenExtendHttpResponseFilter());
                 //HttpFilterHelper.INSTANCE.addFilter(new HttpFilterEntity(new StatisticsRequestFilter(),"gateway.statistics", 1,new HttpFile("gw_statistics.txt")));
     		}
     	});
