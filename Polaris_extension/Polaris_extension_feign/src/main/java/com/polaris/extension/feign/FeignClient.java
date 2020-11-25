b9a7ac1990486e87651588a0fe55654949ed7e6c
yfh0918 <yfh0919@icloud.com>
2020-11-25 10:42:37 +0800

    代码优化

--- Polaris_extension/Polaris_extension_feign/src/main/java/com/polaris/extension/feign/FeignClient.java
+++ Polaris_extension/Polaris_extension_feign/src/main/java/com/polaris/extension/feign/FeignClient.java
@@ -1,12 +1,18 @@
 package com.polaris.extension.feign;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.http.client.HttpClient;
 
+import com.polaris.core.GlobalContext;
 import com.polaris.core.naming.NamingClient;
 import com.polaris.core.util.HttpClientUtil;
 
 import feign.Feign;
 import feign.Request.Options;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import feign.Retryer;
 import feign.httpclient.ApacheHttpClient;
 import feign.jackson.JacksonDecoder;
@@ -14,33 +20,33 @@ import feign.jackson.JacksonEncoder;
 
 public class FeignClient {
     
-    //不启用httpclient的retry
+    //不启用CloseableHttpClient的retry
     private static HttpClient defaultHttpClient = HttpClientUtil.createHttpClient(100, 20, 0);
 
     
-    public static <T> T target(Class<T> apiType, String url) {
-        return target(apiType,null,null,null,url);
+    public static <T> T target(Class<T> apiType, String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,null,null,null,url,requestInterceptors);
     }
-    public static <T> T target(Class<T> apiType, Retryer retryer, String url) {
-        return target(apiType,retryer,null,null,url);
+    public static <T> T target(Class<T> apiType, Retryer retryer, String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,retryer,null,null,url,requestInterceptors);
     }
-    public static <T> T target(Class<T> apiType, Options option, String url) {
-        return target(apiType,null,option,null,url);
+    public static <T> T target(Class<T> apiType, Options option, String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,null,option,null,url,requestInterceptors);
     }
-    public static <T> T target(Class<T> apiType, Retryer retryer,Options option, String url) {
-        return target(apiType,retryer,option,null,url);
+    public static <T> T target(Class<T> apiType, Retryer retryer,Options option, String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,retryer,option,null,url,requestInterceptors);
     }
     
-    public static <T> T target(Class<T> apiType, HttpClient httpClient, String url) {
-        return target(apiType,null,null,httpClient,url);
+    public static <T> T target(Class<T> apiType, HttpClient httpClient, String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,null,null,httpClient,url,requestInterceptors);
     }
-    public static <T> T target(Class<T> apiType, Retryer retryer, HttpClient httpClient,String url) {
-        return target(apiType,retryer,null,httpClient,url);
+    public static <T> T target(Class<T> apiType, Retryer retryer, HttpClient httpClient,String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,retryer,null,httpClient,url,requestInterceptors);
     }
-    public static <T> T target(Class<T> apiType, Options option, HttpClient httpClient,String url) {
-        return target(apiType,null,option,httpClient,url);
+    public static <T> T target(Class<T> apiType, Options option, HttpClient httpClient,String url,RequestInterceptor... requestInterceptors) {
+        return target(apiType,null,option,httpClient,url,requestInterceptors);
     }
-    public static <T> T target(Class<T> apiType, Retryer retryer,Options option, HttpClient httpClient, String url) {
+    public static <T> T target(Class<T> apiType, Retryer retryer,Options option,  HttpClient httpClient, String url,RequestInterceptor... requestInterceptors) {
         if (retryer == null) {
             retryer = new Retryer.Default();
         }
@@ -50,13 +56,29 @@ public class FeignClient {
         if (httpClient == null) {
             httpClient = defaultHttpClient;
         }
+        Set<RequestInterceptor> requestInterceptorSet = new HashSet<>();
+        requestInterceptorSet.add(new TraceInterceptor());
+        if (requestInterceptors != null) {
+            for (RequestInterceptor requestInterceptor : requestInterceptors) {
+                requestInterceptorSet.add(requestInterceptor);
+            }
+        }
         return Feign.builder()
                 .client(new ApacheHttpClient(httpClient))
+                .requestInterceptors(requestInterceptorSet)
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
                 .retryer(retryer)
                 .options(option).target(apiType, NamingClient.getRealIpUrl(url));
     }
-     
+    
+    private static class TraceInterceptor implements RequestInterceptor {
+        
+        @Override
+        public void apply(RequestTemplate requestTemplate) {
+            requestTemplate.header(GlobalContext.TRACE_ID, GlobalContext.getTraceId());
+            requestTemplate.header(GlobalContext.SPAN_ID, GlobalContext.getSpanId());
+        }
+    }
     
 }
